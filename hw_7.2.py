89666628b60adde965ccfe91b0cb33ae76151c43
Vadym <vadimiaroshenko2109@gmail.com>
2024-06-15 12:02:22 +0300

    task2 updated

--- hw_7.2.py
+++ hw_7.2.py
@@ -10,7 +10,4 @@ assert correct_sentence("hello") == "Hello.", 'Test2'
 assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3' 
 assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4' 
 assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5' 
-print('ОК')
-
-sentence = input('Enter sentence: ')
-print(correct_sentence(sentence))
+print('ОК - Task2')
